d483352b9a3d61018cc692131d5b7d2620c324f0
chuchengfeixu <chuchengfeixu@163.com>
2016-08-12 16:34:43 +0800

    add 2.4.4 codes

---
+++ Chap2Fundamentals/2.4.4ImageInterpolation-DingHao/code/Nearest-resize/CMakeFiles/processing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/processing.dir/main.cpp.o"
+  "processing.pdb"
+  "processing"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/processing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
