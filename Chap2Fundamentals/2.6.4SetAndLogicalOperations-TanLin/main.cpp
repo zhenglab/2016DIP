e2d6e25ceea7df6c01483b8368d2622075604c9f
“tanlin816” <1464133184@qq.com>
2016-09-01 21:09:10 +0800

    modify the codes of logical operations

--- Chap2Fundamentals/2.6.4SetAndLogicalOperations-TanLin/main.cpp
+++ Chap2Fundamentals/2.6.4SetAndLogicalOperations-TanLin/main.cpp
@@ -17,81 +17,60 @@ int main()
 	dstimage2.create(srcimage.rows,srcimage.cols,srcimage.type());
         Mat dstimage3;
 	dstimage3.create(srcimage.rows,srcimage.cols,srcimage.type());
-	   int r = srcimage.rows;  //行数
-	   int c = srcimage.cols;  //列数
-
-	    for(int i = 0;i < r;i++)  
-		{  
-		   for(int j = 0;j < c;j++)  
-			{  	
-		
-			  dstimage.at<Vec3b>(i,j)[0] = 255-srcimage.at<Vec3b>(i,j)[0] ;  //蓝色通道
-			  dstimage.at<Vec3b>(i,j)[1] = 255-srcimage.at<Vec3b>(i,j)[1] ;  //绿色通道
-			  dstimage.at<Vec3b>(i,j)[2] = 255-srcimage.at<Vec3b>(i,j)[2];  //红是通道
+	int r = srcimage.rows;  //行数
+	int c = srcimage.cols;  //列数
+	for(int i = 0;i < r;i++){  
+		for(int j = 0;j < c;j++){  	
+			dstimage.at<Vec3b>(i,j)[0] = 255-srcimage.at<Vec3b>(i,j)[0] ;  //蓝色通道
+			dstimage.at<Vec3b>(i,j)[1] = 255-srcimage.at<Vec3b>(i,j)[1] ;  //绿色通道
+			dstimage.at<Vec3b>(i,j)[2] = 255-srcimage.at<Vec3b>(i,j)[2];  //红是通道
 			
-			}       
-		} 
-           imshow("xiaogutu",dstimage);
-           imwrite("xiaogutu.jpg",dstimage); 
-           CvScalar  sum; 
-           sum=0; 
-           for(int k = 0;k < r;k++)                                             
-           {
-              for(int w = 0;w < c;w++)                                          
-              {                                        
-                for(int x = 0;x < r;x++)                                             
-                {
-                  for(int y = 0;y < c;y++)                                          
-                  {
-                                                                   
-                    sum.val[0] = sum.val[0] + srcimage.at<Vec3b>(x,y)[0]; //蓝色通道求和；
+		}       
+	} 
+        imshow("xiaogutu",dstimage);
+        imwrite("xiaogutu.jpg",dstimage); 
+        CvScalar  sum; 
+        sum=0;                                      
+        for(int x = 0;x < r;x++){
+        	for(int y = 0;y < c;y++){
+                                                                  
+                	sum.val[0] = sum.val[0] + srcimage.at<Vec3b>(x,y)[0]; //蓝色通道求和；
                
-                    sum.val[1] = sum.val[1] + srcimage.at<Vec3b>(x,y)[1]; //绿色通道求和；
+                    	sum.val[1] = sum.val[1] + srcimage.at<Vec3b>(x,y)[1]; //绿色通道求和；
                 
-                    sum.val[2] = sum.val[2] + srcimage.at<Vec3b>(x,y)[2]; //红色通道求和；
+                    	sum.val[2] = sum.val[2] + srcimage.at<Vec3b>(x,y)[2]; //红色通道求和；
                 
                
-                  }
-                } 
-                
-                dstimage2.at<Vec3b>(k,w)[0] = 3*sum.val[0]/(r*c)*srcimage.at<Vec3b>(k,w)[0];          
-                dstimage2.at<Vec3b>(k,w)[1] = 3*sum.val[1]/(r*c)*srcimage.at<Vec3b>(k,w)[1];          
-                dstimage2.at<Vec3b>(k,w)[2] = 3*sum.val[2]/(r*c)*srcimage.at<Vec3b>(k,w)[2];          
+              	}
+      	} 
+    	 for(int k = 0;k < r;k++){
+              	for(int w = 0;w < c;w++)                                          
+             {     
+                dstimage2.at<Vec3b>(k,w)[0] = 3*(sum.val[0]/(r*c));          
+                dstimage2.at<Vec3b>(k,w)[1] = 3*(sum.val[1]/(r*c));          
+                dstimage2.at<Vec3b>(k,w)[2] = 3*(sum.val[2]/(r*c));          
 
               }
            }
          imshow("pingjun",dstimage2);
          imwrite("pingjun.jpg",dstimage2); 
-         for(int m = 0;m < r;m++)                                             
-           {
-              for(int n = 0;n < c;n++)                                          
-              {                      
-                 if(dstimage2.at<Vec3b>(m,n)[0] >=srcimage.at<Vec3b>(m,n)[0])
-                 {
-                   dstimage3.at<Vec3b>(m,n)[0]  = dstimage2.at<Vec3b>(m,n)[0];
-                 }
-                 else 
-                 {
-                   dstimage3.at<Vec3b>(m,n)[0]  = srcimage.at<Vec3b>(m,n)[0];
-                 }
-                 if(dstimage2.at<Vec3b>(m,n)[1] >=srcimage.at<Vec3b>(m,n)[1])
-                 {
-                   dstimage3.at<Vec3b>(m,n)[1]  = dstimage2.at<Vec3b>(m,n)[1];
-                 }
-                 else 
-                 {
-                   dstimage3.at<Vec3b>(m,n)[1]  = srcimage.at<Vec3b>(m,n)[1];
-                 }
-                 if(dstimage2.at<Vec3b>(m,n)[2] >=srcimage.at<Vec3b>(m,n)[2])
-                 {
-                   dstimage3.at<Vec3b>(m,n)[2]  = dstimage2.at<Vec3b>(m,n)[2];
-                 }
-                 else 
-                 {
-                   dstimage3.at<Vec3b>(m,n)[2]  = srcimage.at<Vec3b>(m,n)[2];
-                 }
-              }
-           }
+         for(int m = 0;m < r;m++){
+         	for(int n = 0;n < c;n++){                      
+                 	if(dstimage2.at<Vec3b>(m,n)[0] >=srcimage.at<Vec3b>(m,n)[0])
+                 	dstimage3.at<Vec3b>(m,n)[0] = dstimage2.at<Vec3b>(m,n)[0];
+                 	else 
+                	dstimage3.at<Vec3b>(m,n)[0]  = srcimage.at<Vec3b>(m,n)[0];
+                 	if(dstimage2.at<Vec3b>(m,n)[1] >=srcimage.at<Vec3b>(m,n)[1])
+                 	dstimage3.at<Vec3b>(m,n)[1] = dstimage2.at<Vec3b>(m,n)[1];
+                 	else 
+                	dstimage3.at<Vec3b>(m,n)[1] = srcimage.at<Vec3b>(m,n)[1];
+                 	if(dstimage2.at<Vec3b>(m,n)[2] >=srcimage.at<Vec3b>(m,n)[2])
+                 	dstimage3.at<Vec3b>(m,n)[2] = dstimage2.at<Vec3b>(m,n)[2];
+                	else 
+                 	dstimage3.at<Vec3b>(m,n)[2] = srcimage.at<Vec3b>(m,n)[2];
+                 
+        	 }
+         }
          imshow("bingji",dstimage3);
          imwrite("bingji.jpg",dstimage3); 
 	 waitKey(0); 
